df89df7c64b34fcc83cc3658605ec0dcb83eb8f2
garciadiaz <30601030+garciadiaz@users.noreply.github.com>
2021-08-15 00:08:35 -0400

    2

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-alert('Hello');
-
 
 function generate() {
+  var imagelogo = document.createElement("imagelogo");
+  imagelogo.src = "./gradschool_logo.png";
   const doc = new docx.Document({
   sections: [{
-  properties: {},
-  children: [
-    new docx.Paragraph({
-      children: [
-      new docx.TextRun("Hello World"),
-      new docx.TextRun({
-      text: "Foo Bar",
-      bold: true,
+    properties: {},
+    children: [
+      new docx.Paragraph({
+        children: [
+          new docx.ImageRun({
+              data: imagelogo,
+              transformation: {
+                  width: 200,
+                  height: 200,
+              },
+              floating: {
+                  horizontalPosition: {
+                      offset: 1014400,
+                  },
+                  verticalPosition: {
+                      offset: 1014400,
+                  },
+              },
+          }),
+          new docx.TextRun({
+            text: document.getElementById('ccode').value,
+            bold: true,
+
                   }),
-    new docx.TextRun({
-      text: "\tGithub is the best",
-      bold: true,
+          new docx.TextRun({
+            text: document.getElementById('cname').value,
+            bold: true,
                   }),
                 ],
               }),
@@ -23,9 +38,10 @@ function generate() {
           }]
         });
 
-      docx.Packer.toBlob(doc).then(blob => {
-          console.log(blob);
-          saveAs(blob, "example.docx");
-          console.log("Document created successfully");
+  docx.Packer.toBlob(doc).then(blob => {
+    console.log(blob);
+    saveAs(blob, "example.docx");
+    console.log("Document created successfully");
+    console.log(fld);
         });
       }
